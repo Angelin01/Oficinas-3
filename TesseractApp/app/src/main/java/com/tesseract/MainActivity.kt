984ee312c1848f8792148a6c74a9f4a3e6541718
Davi Boberg <daviboberg@gmail.com>
2018-10-01 21:19:25 -0300

    Navigation on fragments and some icons

--- TesseractApp/app/src/main/java/com/tesseract/MainActivity.kt
+++ TesseractApp/app/src/main/java/com/tesseract/MainActivity.kt
@@ -2,11 +2,52 @@ package com.tesseract
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mMainNav: BottomNavigationView = findViewById<BottomNavigationView>(R.id.home_nav_bar)
+
+        setFragment(HomeFragment() as Fragment)
+
+        mMainNav.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    setFragment(HomeFragment() as Fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_light -> {
+                    setFragment(LightFragment() as Fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_spotify -> {
+                    setFragment(SpotifyFragment() as Fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.nav_about -> {
+                    setFragment(AboutFragment() as Fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    setFragment(HomeFragment() as Fragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+        }
+
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.home_view_frame, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
     }
 }
