2abbca802ed00e8a9cfb768aebf448bdef8687cf
Angelin01 <angeloborsoiross@hotmail.com>
2018-10-14 16:40:34 -0300

    Random now uses numpy because that's apparently faster

--- LEDstripController/LightFunctions/random.py
+++ LEDstripController/LightFunctions/random.py
@@ -1,19 +1,24 @@
-import random
-from sys import maxsize
+from numpy.random import randint
 
 
-def gen_random(n_steps: int = 20, seed: int = None):
+def gen_random(n_leds: int, n_steps: int = 20):
     """
     Generates random leds blinking randomly for random reasons.
     :param n_leds: The number of LEDs to blink.
     :param n_steps: How many different colors per LED to generate. If this number is too low, the blinking lights will
                     appear to be a pattern (which it is) instead of random.
-    :param seed: The seed for the generator. If None, will use the system time.
-    :return: an array of random seeds for generating the tuples
+    :return: a numpy array of GRB values for each led for each step
     """
-    return [random.randrange(maxsize) for _ in range(n_steps)]
+    return randint(256, size=(n_steps, n_leds, 3))
 
 
-def random_update_all(values_sequence, step, n_led):
-    random.seed(values_sequence[step])
-    return [(random.randrange(256), random.randrange(256), random.randrange(256)) for _ in range(n_led)]
+def random_update_all(values_sequence, step, __):
+    """
+    Simple updater for a random
+    Ignores the regular n_leds parameter, since it the gen_random already uses it
+    :param values_sequence: a johnson_gradient value sequence.
+    :param step: Which of the values to return in the sequence.
+    :param __: Normally the number of LEDs. Not used, for compatibility purposes only.
+    :return: The sequence to use for the LEDs.
+    """
+    return values_sequence[step]
