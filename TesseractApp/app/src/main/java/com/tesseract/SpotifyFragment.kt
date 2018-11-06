3976eb841d4602549090bbb6ab6835306cfd2980
Lucas Kaminski <lucaskfreitas@hotmail.com>
2018-11-06 07:34:21 -0200

    Tentando me entender com o Android Studio - criei um botão na tela de Spotify

--- TesseractApp/app/src/main/java/com/tesseract/SpotifyFragment.kt
+++ TesseractApp/app/src/main/java/com/tesseract/SpotifyFragment.kt
@@ -3,6 +3,7 @@ package com.tesseract
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.widget.Button
 import android.view.View
 import android.view.ViewGroup
 
@@ -11,8 +12,15 @@ class SpotifyFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spotify, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_spotify, container, false)
+        configureButtonConnect(view)
+        return view
     }
 
+    private fun configureButtonConnect(view: View) {
+        val buttonConnectWifi: Button = view.findViewById(R.id.buttonSpotifyConnect)
+        buttonConnectWifi.setOnClickListener {
 
+        }
+    }
 }
