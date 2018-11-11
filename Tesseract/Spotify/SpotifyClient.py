e7e8d314b74a7787f50f85c7140e9d7764f7fb60
Lucas Kaminski <lucaskfreitas@hotmail.com>
2018-11-11 19:04:51 -0200

    Main code is now calling the Accelerometer thread

---
+++ Tesseract/Spotify/SpotifyClient.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class SpotifyClient():
+
+	def __init__(self):
+		print('Spotify init')
+		# TODO: Token must be received from Bluetooth
+		self.token = 'BQCYevuKiMyi_iDe9odDdcYqQYvMy5gKroN4bQm2UVSXE-rqXRR-xhhIx-EJVS7skVKMp7EjcRRRw_f10sP39XKMRFbgcCRRJ4Hgms39jYMgRDIXRELWmGG5HN9mteOWaQDKx8XpasYhs_EwS8nITqTFYx0o7Mc'
+
+
+	def make_header(self):
+		header = { "Authorization" : "Bearer " + self.token }
+		return header
+
+
+	def next_track(self):
+		print('next')
+		r = requests.post('https://api.spotify.com/v1/me/player/next', headers = self.make_header())
+		print(r.text)
+
+
+	def previous_track(self):
+		r = requests.post('https://api.spotify.com/v1/me/player/previous', headers = self.make_header())
--- Tesseract/Tesseract.py
+++ Tesseract/Tesseract.py
@@ -1,8 +1,15 @@
+#!/usr/bin/python
+
+from Accelerometer.AccThread import AccThread
+
 
 class Tesseract:
 
 	def run(self):
-		pass
+		# TODO: Create bluetooth thread
+		# TODO: Create LED control thread
+		acc_thread = AccThread()
+		acc_thread.run()
 
 
 if __name__ == '__main__':
