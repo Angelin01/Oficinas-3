73cf0db8a4f6dbfbc6983859d978be008d9b1c6e
Davi Boberg <daviboberg@gmail.com>
2018-11-14 22:12:45 -0200

    Add frequency analyzer to main project

---
+++ Tesseract/Audio/Audio.py
@@ -0,0 +1,36 @@
+import pyaudio
+import struct
+import numpy as np
+
+chunk_size = 1024 * 2
+data_format = pyaudio.paInt16
+n_channels = 2
+sample_rate = 44100
+
+p = pyaudio.PyAudio()
+
+input_device_index = 2
+
+stream = p.open(format=data_format,
+                channels=n_channels,
+                rate=sample_rate,
+                input=True,
+                frames_per_buffer=chunk_size,
+                input_device_index=input_device_index)
+
+
+def getLoopbackAudioData():
+	raw_data = stream.read(chunk_size, exception_on_overflow=False)
+
+	# Convert raw sound data to Numpy array
+	fmt = "%dH" % (len(raw_data) / 2)
+	data = struct.unpack(fmt, raw_data)
+	data = np.array(data, dtype='h')
+
+	return data
+
+
+def stopLoopbackAudioStream():
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
---
+++ Tesseract/FrequencyAnalyzer/FrequencyAnalyzer.py
@@ -0,0 +1,59 @@
+import scipy.fftpack
+from rpi_audio_levels import AudioLevels
+import numpy as np
+from Audio import Audio
+
+
+def calculateFFT(music_samples, chunk_size, n_bands=24, bands_intervals=[], using_scipy=True,
+                 sample_rate=Audio.sample_rate):
+	window = np.hanning(0)
+
+	if len(music_samples) != len(window):
+		window = np.hanning(len(music_samples)).astype(np.float32)
+
+	samples_windowed = music_samples * window
+
+	if using_scipy:
+		fourier = scipy.fftpack.fft(samples_windowed)
+	else:
+		fourier = gpu(samples_windowed)
+
+	fourier = np.abs(fourier[:chunk_size // 2])
+	fourier = fourier ** 2 / chunk_size
+	n_bands, frequencies = bands(fourier, n_bands, sample_rate)
+
+	return n_bands, frequencies
+
+
+def gpu(data):
+	DATA_SIZE = 11
+	BANDS_COUNT = len(data)
+	audio_levels = AudioLevels(DATA_SIZE, BANDS_COUNT)
+
+	bands_indexes = [[i, i + 1] for i in range(1024)]
+	new_data = []
+	for i in data:
+		new_data.append(float(i))
+
+	data = np.array(new_data, dtype=np.float32)
+	levels, _, _ = audio_levels.compute(data, bands_indexes)
+	return levels
+
+
+def bands(fourier, n_bands, sample_rate):
+	frequencies = []
+	levels = []
+	max_frequency = sample_rate // 2
+	bandwidth = max_frequency // n_bands
+	points = int(np.floor(len(fourier) // n_bands))
+	for band in range(n_bands):
+		low_frequency = band * bandwidth
+		high_frequency = low_frequency + bandwidth
+
+		frequencies.append((high_frequency + low_frequency) // 2)
+		points_for_band = fourier[band * points: band * points + points]
+
+		level = sum(points_for_band)
+		levels.append(level)
+
+	return levels, frequencies
