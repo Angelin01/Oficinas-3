4257ce9cf64d87ce024c0a1267ab89d5e65e2d4d
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-20 00:23:23 -0200

    Added extra whitelines fix for wpa_supplicant,conf
    Also added a blank line at the end

--- Tesseract/Tesseract.sh
+++ Tesseract/Tesseract.sh
@@ -1,1 +1,5 @@
+FIXWPA=$(cat -s /etc/wpa_supplicant/wpa_supplicant.conf)
+/bin/echo "$FIXWPA" > /etc/wpa_supplicant/wpa_supplicant.conf
+/sbin/wpa_cli -i wlan0 reconfigure
+
 /usr/bin/python3 /home/pi/Tesseract/Tesseract.py
